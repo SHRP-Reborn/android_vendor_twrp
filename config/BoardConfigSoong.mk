8610bf4391c8903e64c4d9c03b628c46f7c9daf5
Rostislav Lastochkin <remtrik@mail.ru>
2026-05-29 11:38:20 +0300

    SHRP: Add dark theme and lite theme vars

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -2,82 +2,82 @@ PATH_OVERRIDE_SOONG := $(shell echo $(TOOLS_PATH_OVERRIDE))
 
 # Add variables that we wish to make available to soong here.
 EXPORT_TO_SOONG := \
-    KERNEL_ARCH \
-    KERNEL_BUILD_OUT_PREFIX \
-    KERNEL_CROSS_COMPILE \
-    KERNEL_MAKE_CMD \
-    KERNEL_MAKE_FLAGS \
-    PATH_OVERRIDE_SOONG \
-    TARGET_KERNEL_CONFIG \
-    TARGET_KERNEL_HEADERS_SOURCE \
-    TW_THEME \
-    TW_CUSTOM_THEME \
-    DEVICE_RESOLUTION \
-    TARGET_SCREEN_WIDTH \
-    TARGET_SCREEN_HEIGHT \
-    TARGET_RECOVERY_PIXEL_FORMAT \
-    TW_EXTRA_LANGUAGES \
-    TWRP_CUSTOM_KEYBOARD \
-    AB_OTA_UPDATER \
-    TW_TARGET_USES_QCOM_BSP \
-    TARGET_RECOVERY_FORCE_PIXEL_FORMAT \
-    TW_INCLUDE_JPEG \
-    TW_ROTATION \
-    BOARD_HAS_FLIPPED_SCREEN \
-    TW_HAPTICS_TSPDRV \
-    TW_SUPPORT_INPUT_AIDL_HAPTICS \
-    TW_SUPPORT_INPUT_1_2_HAPTICS \
-    TARGET_PREBUILT_KERNEL \
-    TW_STATUS_ICONS_ALIGN \
-    TW_CUSTOM_BATTERY_POS \
-    TW_CUSTOM_CLOCK_POS \
-    TW_CUSTOM_CPU_POS \
-    SHRP_CUSTOM_FLASHLIGHT \
-    SHRP_DEV_USE_HEX \
-    SHRP_DEV_FLASH_BOTH_SLOTS \
-    SHRP_EXPRESS \
-    SHRP_EXPRESS_USE_DATA \
-    SHRP_BUILD_DATE \
-    SHRP_DATE \
-    SHRP_OFFICIAL \
-    IS_OFFICIAL \
-    SHRP_AB \
-    SHPR_HAS_RECOVERY_PARTITION \
-    SHRP_SKIP_DEFAULT_ADDON_1 \
-    SHRP_SKIP_DEFAULT_ADDON_2 \
-    SHRP_SKIP_DEFAULT_ADDON_3 \
-    SHRP_SKIP_DEFAULT_ADDON_4 \
-    SHRP_EXCLUDE_MAGISK_FLASH \
-    SHRP_EXTERNAL_ADDON_1_NAME \
-    SHRP_EXTERNAL_ADDON_1_INFO \
-    SHRP_EXTERNAL_ADDON_1_FILENAME \
-    SHRP_EXTERNAL_ADDON_1_BTN_TEXT \
-    SHRP_EXTERNAL_ADDON_1_SUCCESSFUL_TEXT \
-    SHRP_EXTERNAL_ADDON_2_NAME \
-    SHRP_EXTERNAL_ADDON_2_INFO \
-    SHRP_EXTERNAL_ADDON_2_FILENAME \
-    SHRP_EXTERNAL_ADDON_2_BTN_TEXT \
-    SHRP_EXTERNAL_ADDON_2_SUCCESSFUL_TEXT \
-    SHRP_EXTERNAL_ADDON_3_NAME \
-    SHRP_EXTERNAL_ADDON_3_INFO \
-    SHRP_EXTERNAL_ADDON_3_FILENAME \
-    SHRP_EXTERNAL_ADDON_3_BTN_TEXT \
-    SHRP_EXTERNAL_ADDON_3_SUCCESSFUL_TEXT \
-    SHRP_EXTERNAL_ADDON_4_NAME \
-    SHRP_EXTERNAL_ADDON_4_INFO \
-    SHRP_EXTERNAL_ADDON_4_FILENAME \
-    SHRP_EXTERNAL_ADDON_4_BTN_TEXT \
-    SHRP_EXTERNAL_ADDON_4_SUCCESSFUL_TEXT \
-    SHRP_EXTERNAL_ADDON_5_NAME \
-    SHRP_EXTERNAL_ADDON_5_INFO \
-    SHRP_EXTERNAL_ADDON_5_FILENAME \
-    SHRP_EXTERNAL_ADDON_5_BTN_TEXT \
-    SHRP_EXTERNAL_ADDON_5_SUCCESSFUL_TEXT \
-    SHRP_EXTERNAL_ADDON_6_NAME \
-    SHRP_EXTERNAL_ADDON_6_INFO \
-    SHRP_EXTERNAL_ADDON_6_FILENAME \
-    SHRP_EXTERNAL_ADDON_6_BTN_TEXT \
-    SHRP_EXTERNAL_ADDON_6_SUCCESSFUL_TEXT
+		KERNEL_ARCH \
+		KERNEL_BUILD_OUT_PREFIX \
+		KERNEL_CROSS_COMPILE \
+		KERNEL_MAKE_CMD \
+		KERNEL_MAKE_FLAGS \
+		PATH_OVERRIDE_SOONG \
+		TARGET_KERNEL_CONFIG \
+		TARGET_KERNEL_HEADERS_SOURCE \
+		TW_THEME \
+		TW_CUSTOM_THEME \
+		DEVICE_RESOLUTION \
+		TARGET_SCREEN_WIDTH \
+		TARGET_SCREEN_HEIGHT \
+		TARGET_RECOVERY_PIXEL_FORMAT \
+		TW_EXTRA_LANGUAGES \
+		TWRP_CUSTOM_KEYBOARD \
+		AB_OTA_UPDATER \
+		TW_TARGET_USES_QCOM_BSP \
+		TARGET_RECOVERY_FORCE_PIXEL_FORMAT \
+		TW_INCLUDE_JPEG \
+		TW_ROTATION \
+		BOARD_HAS_FLIPPED_SCREEN \
+		TW_HAPTICS_TSPDRV \
+		TW_SUPPORT_INPUT_AIDL_HAPTICS \
+		TW_SUPPORT_INPUT_1_2_HAPTICS \
+		TARGET_PREBUILT_KERNEL \
+		TW_STATUS_ICONS_ALIGN \
+		TW_CUSTOM_BATTERY_POS \
+		TW_CUSTOM_CLOCK_POS \
+		TW_CUSTOM_CPU_POS \
+		SHRP_CUSTOM_FLASHLIGHT \
+		SHRP_DEV_USE_HEX \
+		SHRP_DEV_FLASH_BOTH_SLOTS \
+		SHRP_EXPRESS \
+		SHRP_EXPRESS_USE_DATA \
+		SHRP_BUILD_DATE \
+		SHRP_DATE \
+		SHRP_OFFICIAL \
+		IS_OFFICIAL \
+		SHRP_AB \
+		SHPR_HAS_RECOVERY_PARTITION \
+		SHRP_SKIP_DEFAULT_ADDON_1 \
+		SHRP_SKIP_DEFAULT_ADDON_2 \
+		SHRP_SKIP_DEFAULT_ADDON_3 \
+		SHRP_SKIP_DEFAULT_ADDON_4 \
+		SHRP_EXCLUDE_MAGISK_FLASH \
+		SHRP_EXTERNAL_ADDON_1_NAME \
+		SHRP_EXTERNAL_ADDON_1_INFO \
+		SHRP_EXTERNAL_ADDON_1_FILENAME \
+		SHRP_EXTERNAL_ADDON_1_BTN_TEXT \
+		SHRP_EXTERNAL_ADDON_1_SUCCESSFUL_TEXT \
+		SHRP_EXTERNAL_ADDON_2_NAME \
+		SHRP_EXTERNAL_ADDON_2_INFO \
+		SHRP_EXTERNAL_ADDON_2_FILENAME \
+		SHRP_EXTERNAL_ADDON_2_BTN_TEXT \
+		SHRP_EXTERNAL_ADDON_2_SUCCESSFUL_TEXT \
+		SHRP_EXTERNAL_ADDON_3_NAME \
+		SHRP_EXTERNAL_ADDON_3_INFO \
+		SHRP_EXTERNAL_ADDON_3_FILENAME \
+		SHRP_EXTERNAL_ADDON_3_BTN_TEXT \
+		SHRP_EXTERNAL_ADDON_3_SUCCESSFUL_TEXT \
+		SHRP_EXTERNAL_ADDON_4_NAME \
+		SHRP_EXTERNAL_ADDON_4_INFO \
+		SHRP_EXTERNAL_ADDON_4_FILENAME \
+		SHRP_EXTERNAL_ADDON_4_BTN_TEXT \
+		SHRP_EXTERNAL_ADDON_4_SUCCESSFUL_TEXT \
+		SHRP_EXTERNAL_ADDON_5_NAME \
+		SHRP_EXTERNAL_ADDON_5_INFO \
+		SHRP_EXTERNAL_ADDON_5_FILENAME \
+		SHRP_EXTERNAL_ADDON_5_BTN_TEXT \
+		SHRP_EXTERNAL_ADDON_5_SUCCESSFUL_TEXT \
+		SHRP_EXTERNAL_ADDON_6_NAME \
+		SHRP_EXTERNAL_ADDON_6_INFO \
+		SHRP_EXTERNAL_ADDON_6_FILENAME \
+		SHRP_EXTERNAL_ADDON_6_BTN_TEXT \
+		SHRP_EXTERNAL_ADDON_6_SUCCESSFUL_TEXT
 
 # Setup SOONG_CONFIG_* vars to export the vars listed above.
 # Documentation here:
@@ -88,78 +88,80 @@ SOONG_CONFIG_NAMESPACES += twrpVarsPlugin
 SOONG_CONFIG_twrpVarsPlugin :=
 
 define addVar
-  SOONG_CONFIG_twrpVarsPlugin += $(1)
-  SOONG_CONFIG_twrpVarsPlugin_$(1) := $$(subst ",\",$$($1))
+	SOONG_CONFIG_twrpVarsPlugin += $(1)
+	SOONG_CONFIG_twrpVarsPlugin_$(1) := $$(subst ",\",$$($1))
 endef
 
 $(foreach v,$(EXPORT_TO_SOONG),$(eval $(call addVar,$(v))))
 
 SOONG_CONFIG_NAMESPACES += twrpGlobalVars
 SOONG_CONFIG_twrpGlobalVars += \
-    legacy_hw_disk_encryption \
-    target_enforce_ab_ota_partition_list \
-    target_init_vendor_lib \
-    supports_hw_fde \
-    supports_hw_fde_perf \
-    tw_delay_touch_init_ms \
-    tw_event_logging \
-    tw_use_key_code_touch_sync \
-    tw_ozip_decrypt_key \
-    tw_no_screen_blank \
-    tw_no_screen_timeout \
-    tw_oem_build \
-    tw_x_offset \
-    tw_y_offset \
-    tw_w_offset \
-    tw_h_offset \
-    tw_framerate \
-    tw_round_screen \
-    twrp_custom_keyboard \
-    tw_support_input_1_2_haptics \
-    tw_target_uses_qcom_bsp \
-    tw_include_jpeg \
-    recovery_touchscreen_swap_xy \
-    recovery_touchscreen_flip_x \
-    recovery_touchscreen_flip_y \
-    recovery_graphics_force_use_linelength \
-    recovery_graphics_force_single_buffer \
-    twrp_event_logging \
-    target_recovery_overscan_percent \
-    tw_screen_blank_on_boot \
-    tw_fbiopan \
-    tw_ignore_major_axis_0 \
-    tw_ignore_mt_position_0 \
-    tw_input_blacklist \
-    tw_whitelist_input \
-    tw_haptics_tspdrv \
-    tw_exclude_nano \
-    tw_support_input_aidl_haptics \
-    tw_support_input_aidl_haptics_fqname \
-    tw_support_input_aidl_haptics_fix_off \
-    tw_use_samsung_haptics \
-    tw_brightness_path \
-    tw_max_brightness  \
-    shrp_custom_flashlight \
-    shrp_dev_use_hex \
-    shrp_dev_flash_both_slots \
-    shrp_express \
-    shrp_express_use_data \
-    shrp_build_date \
-    shrp_date \
-    shrp_official \
-    is_official \
-    shrp_ab \
-    shrp_has_recovery_partition \
-    shrp_skip_default_addon_1 \
-    shrp_skip_default_addon_2 \
-    shrp_skip_default_addon_3 \
-    shrp_skip_default_addon_4 \
-    shrp_exclude_magisk_flash
+		legacy_hw_disk_encryption \
+		target_enforce_ab_ota_partition_list \
+		target_init_vendor_lib \
+		supports_hw_fde \
+		supports_hw_fde_perf \
+		tw_delay_touch_init_ms \
+		tw_event_logging \
+		tw_use_key_code_touch_sync \
+		tw_ozip_decrypt_key \
+		tw_no_screen_blank \
+		tw_no_screen_timeout \
+		tw_oem_build \
+		tw_x_offset \
+		tw_y_offset \
+		tw_w_offset \
+		tw_h_offset \
+		tw_framerate \
+		tw_round_screen \
+		twrp_custom_keyboard \
+		tw_support_input_1_2_haptics \
+		tw_target_uses_qcom_bsp \
+		tw_include_jpeg \
+		recovery_touchscreen_swap_xy \
+		recovery_touchscreen_flip_x \
+		recovery_touchscreen_flip_y \
+		recovery_graphics_force_use_linelength \
+		recovery_graphics_force_single_buffer \
+		twrp_event_logging \
+		target_recovery_overscan_percent \
+		tw_screen_blank_on_boot \
+		tw_fbiopan \
+		tw_ignore_major_axis_0 \
+		tw_ignore_mt_position_0 \
+		tw_input_blacklist \
+		tw_whitelist_input \
+		tw_haptics_tspdrv \
+		tw_exclude_nano \
+		tw_support_input_aidl_haptics \
+		tw_support_input_aidl_haptics_fqname \
+		tw_support_input_aidl_haptics_fix_off \
+		tw_use_samsung_haptics \
+		tw_brightness_path \
+		tw_max_brightness  \
+		shrp_custom_flashlight \
+		shrp_dev_use_hex \
+		shrp_dev_flash_both_slots \
+		shrp_express \
+		shrp_express_use_data \
+		shrp_build_date \
+		shrp_date \
+		shrp_official \
+		is_official \
+		shrp_ab \
+		shrp_has_recovery_partition \
+		shrp_dark \
+		shrp_lite \
+		shrp_skip_default_addon_1 \
+		shrp_skip_default_addon_2 \
+		shrp_skip_default_addon_3 \
+		shrp_skip_default_addon_4 \
+		shrp_exclude_magisk_flash
 
 ifeq ($(TARGET_HW_DISK_ENCRYPTION),true)
 SOONG_CONFIG_twrpGlobalVars += \
-    hw_fde_cryptfs_hw_header_lib_name \
-    hw_fde_cryptfs_hw_shared_lib_name
+		hw_fde_cryptfs_hw_header_lib_name \
+		hw_fde_cryptfs_hw_shared_lib_name
 endif
 
 # Defaults
@@ -217,19 +219,19 @@ SOONG_CONFIG_twrpGlobalVars_tw_support_input_aidl_haptics_fix_off := $(TW_SUPPOR
 SOONG_CONFIG_twrpGlobalVars_tw_use_samsung_haptics := $(TW_USE_SAMSUNG_HAPTICS)
 SOONG_CONFIG_twrpGlobalVars_tw_brightness_path := $(subst ",, $(TW_BRIGHTNESS_PATH))
 ifneq ($(TW_NO_SCREEN_BLANK),)
-  ifeq ($(TW_MAX_BRIGHTNESS),)
-    TW_MAX_BRIGHTNESS := 1023
-    $(call pretty-warning,TW_MAX_BRIGHTNESS must be defined if using TW_NO_SCREEN_BLANK. Setting to default value: $(TW_MAX_BRIGHTNESS). If you would like to use a different value then please update your device tree with the correct value from your device.)
-  endif
+	ifeq ($(TW_MAX_BRIGHTNESS),)
+		TW_MAX_BRIGHTNESS := 1023
+		$(call pretty-warning,TW_MAX_BRIGHTNESS must be defined if using TW_NO_SCREEN_BLANK. Setting to default value: $(TW_MAX_BRIGHTNESS). If you would like to use a different value then please update your device tree with the correct value from your device.)
+	endif
 endif
 SOONG_CONFIG_twrpGlobalVars_tw_max_brightness := $(TW_MAX_BRIGHTNESS)
 
 ifneq ($(TARGET_CRYPTFS_HW_PATH),)
-  SOONG_CONFIG_twrpGlobalVars_hw_fde_cryptfs_hw_header_lib_name := //$(TARGET_CRYPTFS_HW_PATH):libcryptfs_hw_headers
-  SOONG_CONFIG_twrpGlobalVars_hw_fde_cryptfs_hw_shared_lib_name := //$(TARGET_CRYPTFS_HW_PATH):libcryptfs_hw
+	SOONG_CONFIG_twrpGlobalVars_hw_fde_cryptfs_hw_header_lib_name := //$(TARGET_CRYPTFS_HW_PATH):libcryptfs_hw_headers
+	SOONG_CONFIG_twrpGlobalVars_hw_fde_cryptfs_hw_shared_lib_name := //$(TARGET_CRYPTFS_HW_PATH):libcryptfs_hw
 else
-  SOONG_CONFIG_twrpGlobalVars_hw_fde_cryptfs_hw_header_lib_name := libcryptfs_hw_headers
-  SOONG_CONFIG_twrpGlobalVars_hw_fde_cryptfs_hw_shared_lib_name := libcryptfs_hw
+	SOONG_CONFIG_twrpGlobalVars_hw_fde_cryptfs_hw_header_lib_name := libcryptfs_hw_headers
+	SOONG_CONFIG_twrpGlobalVars_hw_fde_cryptfs_hw_shared_lib_name := libcryptfs_hw
 endif
 
 # Set default values
@@ -249,6 +251,8 @@ SOONG_CONFIG_twrpGlobalVars_shrp_official := $(SHRP_OFFICIAL)
 SOONG_CONFIG_twrpGlobalVars_is_official := $(IS_OFFICIAL)
 SOONG_CONFIG_twrpGlobalVars_shrp_ab := $(SHRP_AB)
 SOONG_CONFIG_twrpGlobalVars_shrp_has_recovery_partition := $(SHPR_HAS_RECOVERY_PARTITION)
+SOONG_CONFIG_twrpGlobalVars_shrp_dark := $(SHRP_DARK)
+SOONG_CONFIG_twrpGlobalVars_shrp_lite := $(SHRP_LITE)
 SOONG_CONFIG_twrpGlobalVars_shrp_skip_default_addon_1 := $(SHRP_SKIP_DEFAULT_ADDON_1)
 SOONG_CONFIG_twrpGlobalVars_shrp_skip_default_addon_2 := $(SHRP_SKIP_DEFAULT_ADDON_2)
 SOONG_CONFIG_twrpGlobalVars_shrp_skip_default_addon_3 := $(SHRP_SKIP_DEFAULT_ADDON_3)
